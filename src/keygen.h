fa22d23622e806a9ec58bccc25eac32cec6c10d1
Siyan HU <siyan.dev@gmail.com>
2020-04-13 22:43:31 +0800

    temporal

---
+++ src/keygen.h
@@ -0,0 +1,31 @@
+//
+// Created by HU Siyan on 13/4/2020.
+//
+
+#ifndef KEYGEN_H
+#define KEYGEN_H
+
+#include <string>
+#include <random>
+
+class keygen {
+    struct UUID {
+        unsigned PWD;//16 bits
+        unsigned long long TimeEpoch; //52 bits
+        unsigned long long RandomNumber; //60 bits
+    };
+
+public:
+    keygen();
+    ~keygen();
+    std::string generateKey(unsigned long long timeEpoch, unsigned pwd, bool bestPrint);
+
+private:
+    unsigned long long randomNumGenerater(std::mt19937& PRNG);
+    unsigned long long pwdProcessor(std::mt19937& PRNG);
+    std::string GenerateUUID(bool bPrettyPrint=true);
+    UUID uuid;
+};
+
+
+#endif //KEYGEN_H
